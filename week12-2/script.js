b8b1d2b093889e4ce251983d299b2e2a78a949d6
Capybara303 <xl897@cornell.edu>
2024-11-14 12:24:10 -0500

    Update script.js

--- week12-2/script.js
+++ week12-2/script.js
@@ -1,1 +1,12 @@
-mapboxgl.accessToken = "",
+mapboxgl.accessToken = "pk.eyJ1IjoieGlueWk5OTkiLCJhIjoiY20xc243OXBiMDgxbDJscHVldHF6dnFpcSJ9.EO37NGlWaFpO-c4FDBBVIg",
+
+const map = new mapboxgl.Map({
+	container: "Xinyi",
+	style: 'mapbox://styles/xinyi999/cm3hksf9f00d301rv0pr9hdji',
+    center: [-122.41565, 37.78101],
+    zoom: 12.29,
+    pitch: 30,
+    bearing: 0,
+	minZoom:9
+
+})
